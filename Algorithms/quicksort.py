84ac34057b50108615099be70eb470c0111e4e61
Ben Woo <benedict_woo_jun_kai@s2016.sst.edu.sg>
2019-01-15 22:37:35 +0800

    Quicksort animation completed

--- Algorithms/quicksort.py
+++ Algorithms/quicksort.py
@@ -3,7 +3,7 @@ from random import randint
 
 #Initializing the window
 pygame.init()
-size = (1500,500)
+size = (1000,500)
 window = pygame.display.set_mode((size))
 pygame.display.set_caption("Quick-Sort Visualization")
 black = pygame.Color(0,0,0)
@@ -13,8 +13,7 @@ green = pygame.Color(0,255,0)
  
 #Declaring Variables
 heightList = []
-listLength = 10
-swap = False
+listLength = 20
 
 #coordinate for drawing
 xList,y,w = [],0,size[0]/listLength
@@ -41,16 +40,108 @@ def update_draw():
     pygame.time.Clock().tick(10)
  
 #Algorithm
-# Algorithm
-# While true is needed for the screen to stay there
+def quickSort(heightList):
+   quickSortHelper(heightList,0,len(heightList)-1)
+
+def quickSortHelper(heightList,first,last):
+   if first<last:
+
+       #Its going to Partiition
+       splitpoint = partition(heightList,first,last)
+
+       #Basically, per iteration you are running it  split into 2. So it does it for the
+       #beofre and after the Partition
+       quickSortHelper(heightList,first,splitpoint-1)
+       quickSortHelper(heightList,splitpoint+1,last)
+
+def partition(heightList,first,last):
+   #set a pivot value, in our case at the start
+   pivotvalue = heightList[first]
+
+   #ensure that the border when swapping heightListound stuff, is right of the 
+   border = first+1
+   end = last
+   isCompleted = False
+   swap = False
+
+   while not isCompleted:
+
+       draw()
+       if swap:
+           pygame.draw.rect(window, green, (xList[border],500-heightList[border],w,heightList[border]), 0)
+           pygame.draw.rect(window, green, (xList[end],500-heightList[end],w,heightList[end]), 0)
+           swap = False
+       update_draw()
+       buffer()
+
+       #Basically this is once everythin is sorted and checks if its more
+       while border <= end and heightList[border] <= pivotvalue:
+           border = border + 1
+
+       #Same thing as above but opposite and in this cfase its less
+       while heightList[end] >= pivotvalue and end >= border:
+           end = end -1
+
+       #this is bascially once everything after the border, which in the end gos to the end, is smaller 
+       if end < border:
+           isCompleted = True
+
+       #if not it swaps 
+       else:
+           #Before swaps
+           pygame.draw.rect(window, red, (xList[border],500-heightList[border],w,heightList[border]), 0)
+           pygame.draw.rect(window, red, (xList[end],500-heightList[end],w,heightList[end]), 0)
+           update_draw()
+           buffer()
+
+           heightList[border], heightList[end] = heightList[end], heightList[border]
+           
+           #After swaps
+           draw()
+           pygame.draw.rect(window, green, (xList[border],500-heightList[border],w,heightList[border]), 0)
+           pygame.draw.rect(window, green, (xList[end],500-heightList[end],w,heightList[end]), 0)
+           update_draw()
+           buffer()
+
+
+   #it swaps again
+   pygame.draw.rect(window, red, (xList[first],500-heightList[first],w,heightList[first]), 0)
+   pygame.draw.rect(window, red, (xList[end],500-heightList[end],w,heightList[end]), 0)
+   update_draw()
+   buffer()
+
+   heightList[first], heightList[end] = heightList[end], heightList[first]
+
+   draw()
+   pygame.draw.rect(window, green, (xList[first],500-heightList[first],w,heightList[first]), 0)
+   pygame.draw.rect(window, green, (xList[end],500-heightList[end],w,heightList[end]), 0)
+   update_draw()
+   buffer()
+
+   return end
+
+quickSort(heightList)
+
+#Sort Ended
+
+#End animation
+draw()
+update_draw()
+buffer()
+#green going up
+for i in range(listLength):
+    pygame.draw.rect(window, green, (xList[i],500-heightList[i],w,heightList[i]), 0)
+    update_draw()
+    buffer()
+#green going down
+for i in range(listLength-1, -1, -1):
+    pygame.draw.rect(window, white, (xList[i],500-heightList[i],w,heightList[i]), 0)
+    update_draw()
+    buffer()
+
+#Show results
+print(heightList)
+
+#Ended
 while True:
-	# choose pivot_
-	# 2-way partition_
-	
-
-
-        draw(white)
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+    buffer()
