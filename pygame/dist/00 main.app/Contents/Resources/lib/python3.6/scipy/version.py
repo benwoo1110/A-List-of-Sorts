62a8f388c51594128984f8f4b38f3af95cf5468a
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-06-11 22:20:11 +0800

    updated .gitignore

---
+++ pygame/dist/00 main.app/Contents/Resources/lib/python3.6/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.19.1'
+version = '0.19.1'
+full_version = '0.19.1'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
